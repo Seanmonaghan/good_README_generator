d74ac38cb4156ae9d9145b3e3bfac3d6d7c270a0
Sean Monaghan <smonagha@conncoll.edu>
2021-03-21 18:27:28 -0400

    Successfully wrote the code to write a README.md file with the information I want on it, and formatted how I wanted it.

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# good_README_generator
+
+    # 
+
+    ## Description
+    
+    dsfsd
+    
+    ## Table of Contents
+    
+    [Description](#description)
+    [Installation Instructions](#installation-instructions)
+    [Usage Information](#usage-information)
+    [License](#license)
+    [Contribution Guidelines](#contribution-guidelines)
+    [Tests](#tests)
+    [Questions](#questions)
+    
+    ## Installation Instructions
+
+    adsf
+    
+    ## Usage Information
+
+    adsfsda
+    
+    ## License
+
+    MIT
+    
+    ## Contribution Guidelines
+
+    fdasf
+    
+    ## Tests
+
+    adsf
+    
+    ## Questions
+    fasdfa
+    dasfasd
+        
--- index.js
+++ index.js
@@ -1,54 +1,136 @@
 const inquirer = require('inquirer');
+const fs = require("fs");
+const util = require("util");
 
-inquirer
-    .prompt([{
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+function promptUser() {
+    return inquirer.prompt([{
             type: "input",
-            name: "project Name",
+            name: "title",
             message: "What is the name of your project?"
         },
         {
             type: "input",
-            name: "Description",
+            name: "description",
             message: "Please write out a description of your project?"
         },
         {
             type: "input",
-            name: "Installation Instructions",
+            name: "installation",
             message: "What are the installation instructions?"
         },
         {
             type: "input",
-            name: "Usage Information",
+            name: "usage",
             message: "What is the Usage Information?"
         },
         {
             type: "input",
-            name: "Contribution Guidelines",
+            name: "contribution",
             message: "What are the contribution guidelines?"
         },
         {
             type: "input",
-            name: "Test",
+            name: "test",
             message: "What are the testing instructions?"
         },
         {
             type: "list",
-            name: "License",
+            name: "license",
             message: "Which license would you like to use?",
             choices: [
                 "ISC",
                 "MIT",
-                "The Other One"
+                "Apache-2.0"
             ]
         },
         {
             type: "input",
-            name: "Github Username",
+            name: "github",
             message: "What is your github username?"
         },
         {
             type: "input",
-            name: "Email",
+            name: "email",
             message: "What is your email address?"
         }
-    ])
+    ]);
+}
+
+function generateREADME(answers) {
+    return `
+    # ${answers.title}
+
+    ## Description
+    
+    ${answers.description}
+    
+    ## Table of Contents
+    
+    [Description](#description)
+    [Installation Instructions](#installation-instructions)
+    [Usage Information](#usage-information)
+    [License](#license)
+    [Contribution Guidelines](#contribution-guidelines)
+    [Tests](#tests)
+    [Questions](#questions)
+    
+    ## Installation Instructions
+
+    ${answers.installation}
+    
+    ## Usage Information
+
+    ${answers.usage}
+    
+    ## License
+
+    ${answers.license}
+    
+    ## Contribution Guidelines
+
+    ${answers.contribution}
+    
+    ## Tests
+
+    ${answers.test}
+    
+    ## Questions
+    ${answers.email}
+    ${answers.github}
+        `;
+}
+
+promptUser()
+  .then(function(answers) {
+    const readme = generateREADME(answers);
+
+    return writeFileAsync("README.md", readme);
+  })
+  .then(function() {
+    console.log("Successfully wrote to index.html");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+
+
+
+// function githubLink(username) {
+//     fetch('https://api.github.com/users/' + username)
+//         .then(response => response.json())
+//         .then(console.log(data))
+// }
+
+// fs.writeFile("log.txt", process.argv[2], function (err) {
+
+//     if (err) {
+//         return console.log(err);
+//     }
+
+//     console.log("Success!");
+
+// });
